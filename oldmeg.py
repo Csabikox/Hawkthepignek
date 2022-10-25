9765f3cdd2575ba2325a4a3a7543619ff5f62a63
Szabolcs <116083271+Csabikox@users.noreply.github.com>
2022-10-25 13:56:41 +0200

    Create oldmeg.py

---
+++ oldmeg.py
@@ -0,0 +1,9 @@
+1.Feladat: Kérjünk be a felhasználótól 2 számot
+
+zam1 = float(input("Kérem a számot")
+zam2 = float(input("Kérem a számot")
+
+if 1 > 2
+  print("Ez a szám nagyobb mint,1)
+else:
+ print("Ez a szám nagyobb mint,2)
